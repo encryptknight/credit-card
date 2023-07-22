e9a650e5ab47b5190432c75969feed2d265a7a08
Naveen <thotanaveen406@gmail.com>
2023-07-22 11:37:44 +0530

    validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import CCback from './components/CCback';
+import CCfront from './components/CCfront';
+import Left from './images/left.png';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='image'>
+      <img src={Left} alt='Left'/>
+      </div>
+      <div className='front-card'>
+      <CCfront/>
+      </div>
+      <div className='back-card'>
+        <CCback/>
+      </div>
+
+      <div className='form'>
+        <form>
+          <label>CARDHOLDER NAME</label>
+          <input type='text' id='card-name' placeholder='e.g. Jane Appleseed'/><br/>
+          <p className='warn'>Cardholder name required</p>
+          <br/>
+          <label>CARD NUMBER</label>
+          <input type='text' id='card-number' placeholder='e.g. 1234 5678 9123 0000'/><br/>
+          <p className='warn'>Card number required</p>
+          <br/>
+          <div className='date-cvv'>
+          <div id='date'>
+          <label>EXP.DATE(MM/YY)</label>
+          <input type='number' id='mm' placeholder='MM'/>
+          <input type='number' id='yy' placeholder='YY'/>
+          <p className='warn'>Expiry date must be numeric</p>
+          </div>
+          <div id='cvv'>
+          <label>CVC</label>
+          <input type='number' placeholder='e.g. 123'/><br/>
+          <p className='warn'>CVC must be numeric</p><br/>
+          </div>
+          </div><br/>
+          <div>
+          <button id='btn'>Confirm</button>
+          </div>
+        </form>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/CCback.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ccback from '../images/ccback.png'
+import './CCback.css'
+function CCback() {
+  return (
+    <div className='ccback'>
+        <img src= {ccback} alt='ccback' />
+        <p>000</p>
+    </div>
+  )
+}
+
+export default CCback
